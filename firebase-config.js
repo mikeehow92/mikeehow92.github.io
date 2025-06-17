50220ee7f63eff5d7c1304365e3f232f4f27fd22
mikeehow92 <gamer2022nubes@gmail.com>
2025-06-16 21:15:33 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCR-axayENUg4FFb4jj0uVW2BnfwQ5EiXY",
+  authDomain: "mitienda-c2609.firebaseapp.com",
+  projectId: "mitienda-c2609",
+  storageBucket: "mitienda-c2609.appspot.com", // ¡Corregido! (quita .storage)
+  messagingSenderId: "536746062790",
+  appId: "1:536746062790:web:6e545efbc8f037e36538c7",
+  databaseURL: "https://mitienda-c2609.firebaseio.com" // Añade esto
+};
