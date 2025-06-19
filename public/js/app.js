8510008fa04c206ed5b43931b0b58bc98c910109
mikeehow92 <gamer2022nubes@gmail.com>
2025-06-19 01:32:38 -0600

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -10,12 +10,13 @@ import {
 
 // Configuración Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCR-axayENUg4FFb4jj0uVW2BnfwQ5EiXY",
-  authDomain: "mitienda-c2609.firebaseapp.com",
-  projectId: "mitienda-c2609",
-  storageBucket: "mitienda-c2609.appspot.com",
-  messagingSenderId: "536746062790",
-  appId: "1:536746062790:web:6e545efbc8f037e36538c7"
+  apiKey: "{{FIREBASE_API_KEY}}",
+  authDomain: "{{FIREBASE_AUTH_DOMAIN}}",
+  databaseURL: "{{FIREBASE_DATABASE_URL}}", 
+  projectId: "{{FIREBASE_PROJECT_ID}}",
+  storageBucket: "{{FIREBASE_STORAGE_BUCKET}}",
+  messagingSenderId: "{{FIREBASE_MESSAGING_SENDER_ID}}",
+  appId: "{{FIREBASE_APP_ID}}"
 };
 
 // Inicializar Firebase
