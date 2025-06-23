9d3059d5ffd0514e41154c7892423be5332762ae
mikeehow92 <gamer2022nubes@gmail.com>
2025-06-23 14:06:30 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCR-axayENUg4FFb4jj0uVW2BnfwQ5EiXY",
   authDomain: "mitienda-c2609.firebaseapp.com",
@@ -6,4 +11,21 @@ const firebaseConfig = {
   messagingSenderId: "123456789012",
   appId: "1:123456789012:web:abc123def456"
 };
-export default firebaseConfig;
+
+// Inicialización con verificación de duplicados
+let app;
+try {
+  app = initializeApp(firebaseConfig);
+} catch (error) {
+  if (error.code === 'app/duplicate-app') {
+    // Si ya existe, obtenemos la instancia existente
+    app = getApp();
+  } else {
+    throw error;
+  }
+}
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
