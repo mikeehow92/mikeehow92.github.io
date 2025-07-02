718fc93557a4e2d9bc561c9091d772587661d363
mikeehow92 <gamer2022nubes@gmail.com>
2025-07-01 23:14:11 -0600

    Create .eslintrc.js

---
+++ functions/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+  },
+};
